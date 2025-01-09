d2f71225de05e9e893515682749ec90d0d816f67
Shakil Adnan <shakilahamead488@gmail.com>
2025-01-10 01:31:19 +0600

    Update data.php

--- data.php
+++ data.php
@@ -1,3 +1,13 @@
 <?php
-echo "Shakil Adnan is A new Devloper,he is Learning to Bongo Academy Now its working";
+
+$con = mySqli_connect("localhost","if0_37679408","vb7iyRsMjHV89","if0_37679408_shakil");
+
+if(mySqli_connect.errono()){
+    echo "Couldnot Connect DataBase".mySqli_connect_error();
+}
+else{
+    echo "Db Conncect SuccessFully!!";
+}
+
+
 ?>
